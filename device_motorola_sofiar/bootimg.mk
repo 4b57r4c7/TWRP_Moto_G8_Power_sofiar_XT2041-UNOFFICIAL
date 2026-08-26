8666d72a008f0c45b5db7af2fc2d38b412dd112e
4b57r4c7 <dimg0608@protonmail.com>
2026-08-27 00:40:55 +0300

    Add minimal bootimg.mk - required to exist by TWRP build/make

---
+++ device_motorola_sofiar/bootimg.mk
@@ -0,0 +1,6 @@
+# bootimg.mk - Motorola sofiar
+#
+# TWRP-специфичният build/make (bootable/recovery или build/make/core)
+# изисква този файл да съществува за device-specific boot image
+# packaging правила. Започваме празен - ако нещо конкретно липсва
+# (променлива/target), следващата грешка ще покаже какво точно.
